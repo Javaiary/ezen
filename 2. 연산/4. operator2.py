6dfbf6f2570d9e60e4ae9add9f41e8291c0d36f0
Nari Choi <110982271+Javaiary@users.noreply.github.com>
2022-12-20 11:36:36 +0900

    2. 연산

---
+++ 2. 연산/4. operator2.py
@@ -0,0 +1,21 @@
+# 비교연산
+print( 2 > 3)   #false
+print(15 < 30)  #true
+print(1.5 >= 0)  #true
+print(3 <= 3)   #true
+print("미드소마" == "미드소미") #false
+
+
+# 논리연산
+#
+print( 4 <6 and 10 >=10)   # true and true ==> true
+print("사랑해요" != "사랑해요" or "LG" != "Samsung") #false or true ==> true
+print(not 5 == 5) # not true ==> false
+
+# 멤버십 연산자
+# in - 포함되어 있다
+# not in  - 포함되어 있지 않다
+print("a" in "abc")
+print( "d"  not in "abc")
+
+
